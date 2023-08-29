b481cc82cf2f7a619de299cd2890b4322ea5fe19
Mr kCEO <50420192+KuxCEO@users.noreply.github.com>
2023-08-29 19:45:46 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', function () {
     const maintenanceBanner = document.getElementById('maintenance-banner');
     maintenanceBanner.style.display = 'block';
-    
+
     const navLinks = document.querySelectorAll('nav ul li a');
 
     navLinks.forEach(link => {
@@ -13,6 +13,21 @@ window.addEventListener('load', function () {
                 targetSection.scrollIntoView({ behavior: 'smooth' });
             }
         });
+    });
+
+    const logo = document.querySelector('.logo');
+    const fullSize = '5rem'; // Full size on hover
+    const basicSize = '4rem'; // Basic size
+
+    logo.style.fontSize = basicSize;
+
+    logo.addEventListener('mouseover', function () {
+        logo.style.transition = 'font-size 0.2s'; // Adding a transition for smooth effect
+        logo.style.fontSize = fullSize;
+    });
+
+    logo.addEventListener('mouseout', function () {
+        logo.style.fontSize = basicSize;
     });
 
     // Additional JavaScript functionality if needed
