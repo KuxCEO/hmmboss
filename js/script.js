c6f6abe86ba612a3254d5c29da1971dfc56c2aa7
Mr kCEO <50420192+KuxCEO@users.noreply.github.com>
2023-08-29 17:50:35 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
-const navLinks = document.querySelectorAll('nav ul li a');
+window.addEventListener('load', function () {
+    const maintenanceBanner = document.getElementById('maintenance-banner');
+    maintenanceBanner.style.display = 'block';
+    
+    const navLinks = document.querySelectorAll('nav ul li a');
 
-navLinks.forEach(link => {
-    link.addEventListener('click', e => {
-        e.preventDefault();
-        const targetId = link.getAttribute('href');
-        const targetSection = document.querySelector(targetId);
-        if (targetSection) {
-            targetSection.scrollIntoView({ behavior: 'smooth' });
-        }
+    navLinks.forEach(link => {
+        link.addEventListener('click', e => {
+            e.preventDefault();
+            const targetId = link.getAttribute('href');
+            const targetSection = document.querySelector(targetId);
+            if (targetSection) {
+                targetSection.scrollIntoView({ behavior: 'smooth' });
+            }
+        });
     });
-});
 
-// Additional JavaScript functionality if needed
+    // Additional JavaScript functionality if needed
+});
